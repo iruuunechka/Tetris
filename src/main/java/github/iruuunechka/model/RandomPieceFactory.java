3ebd477eb0861565d8da58f0fda2d7866aa8d367
Arkady Rost <arkrost@gmail.com>
2013-09-01 21:23:32 +0400

    View fix. Now using double buffer

--- src/main/java/github/iruuunechka/model/RandomPieceFactory.java
+++ src/main/java/github/iruuunechka/model/RandomPieceFactory.java
@@ -8,13 +8,6 @@ public class RandomPieceFactory implements PieceFactory {
     private static final int[][][][] piecePool = new int[][][][] {
         // square
         new int[][][] {
-            // 0 rotation
-//            new int[][] {
-//                // x
-//                new int[] {0, 1, 0, 1},
-//                //y
-//                new int[] {0, 0, 1, 1}
-//            }
             new int[][] {
                 //x,y
                 new int[] {0, 0},
@@ -25,21 +18,14 @@ public class RandomPieceFactory implements PieceFactory {
         },
         // line
         new int[][][] {
-            // 0 rotation
             new int[][] {
-//                new int[] {0, 1, 2, 3},
-//                new int[] {0, 0, 0, 0}
                 new int[] {0, 0},
                 new int[] {1, 0},
                 new int[] {2, 0},
                 new int[] {3, 0},
             },
-//          // 1 rotation
+          // 1 rotation
             new int[][] {
-//                //x
-//                new int[] {0, 0, 0, 0},
-//                //y
-//                new int[] {0, 1, 2, 3}
                 new int[] {0, 0},
                 new int[] {0, 1},
                 new int[] {0, 2},
@@ -51,7 +37,6 @@ public class RandomPieceFactory implements PieceFactory {
 
     @Override
     public Piece createPiece() {
-        System.out.println(random.nextInt(piecePool.length));
         return new PieceImpl(piecePool[random.nextInt(piecePool.length)]);
     }
 }
